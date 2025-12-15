0bc8afbd7b9696559c4a3eb3ebc3730ef20f7a95
MrGlaz <danalednev@gmail.com>
2025-12-15 21:50:52 +0300

    второй коммит

--- main.py
+++ main.py
@@ -1,2 +1,153 @@
-print(1)
-print(2)
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+class Player(arcade.Sprite):
+    def __init__(self, image_path="images/npc/player_good_npc.png"):
+        super().__init__(image_path, scale=0.5)
+        self.speed = 5
+        self.sprint_speed = 10
+        self.jump_speed = 10
+        self.physics_engine = None
+        self.can_jump = False
+        self.is_sprinting = False
+        self.facing_right = True
+
+    def setup_physics(self, physics_engine):
+        self.physics_engine = physics_engine
+
+    def update(self):
+        super().update()
+        if self.physics_engine:
+            self.can_jump = self.physics_engine.can_jump()
+
+        if self.change_x > 0:
+            self.scale_x = abs(self.scale_x)
+            self.facing_right = True
+        elif self.change_x < 0:
+            self.scale_x = -abs(self.scale_x)
+            self.facing_right = False
+
+        if self.left < 0:
+            self.left = 0
+            self.change_x = 0
+        if self.right > SCREEN_WIDTH:
+            self.right = SCREEN_WIDTH
+            self.change_x = 0
+
+    def move(self, direction):
+        current_speed = self.sprint_speed if self.is_sprinting else self.speed
+        if direction == "right":
+            self.change_x = current_speed
+        elif direction == "left":
+            self.change_x = -current_speed
+
+    def stop(self):
+        self.change_x = 0
+
+    def jump(self):
+        if self.can_jump and self.physics_engine:
+            self.change_y = self.jump_speed
+            self.can_jump = False
+
+    def sprint(self, is_sprinting):
+        self.is_sprinting = is_sprinting
+        if self.change_x != 0:
+            current_speed = self.sprint_speed if is_sprinting else self.speed
+            self.change_x = abs(self.change_x) / self.change_x * current_speed
+
+class Space_button(arcade.Sprite):
+    def __init__(self, x, y):
+        super().__init__(path_or_texture='images/buttons/Space_butt.png', center_x=x, center_y=y)
+
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Player Example")
+        self.scene = None
+        self.player = None
+        self.physics_engine = None
+        self.left_pressed = False
+        self.right_pressed = False
+        self.shift_pressed = False
+
+    def setup(self):
+        self.scene = arcade.Scene()
+        self.space_button = Space_button(500, 300)
+        self.player = Player()
+        self.player.center_x = 100
+        self.player.center_y = 100
+        self.scene.add_sprite("Player", self.player)
+        self.scene.add_sprite('Space_button', self.space_button)
+
+        platforms = arcade.SpriteList()
+        for x in range(0, 900, 64):
+            platform = arcade.Sprite(":resources:images/tiles/grassMid.png", scale=0.5)
+            platform.center_x = x
+            platform.center_y = 32
+            platforms.append(platform)
+
+        platform1 = arcade.Sprite(":resources:images/tiles/grassMid.png", scale=0.5)
+        platform1.center_x = 600
+        platform1.center_y = 100
+        platforms.append(platform1)
+        platform2 = arcade.Sprite(":resources:images/tiles/grassMid.png", scale=0.5)
+        platform2.center_x = 600
+        platform2.center_y = 160
+        platforms.append(platform2)
+
+        self.scene.add_sprite_list("Platforms", sprite_list=platforms)
+
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player,
+            gravity_constant=0.5,
+            walls=self.scene["Platforms"]
+        )
+        self.player.setup_physics(self.physics_engine)
+
+    def on_draw(self):
+        self.clear()
+        self.scene.draw()
+
+    def on_update(self, delta_time):
+        self.physics_engine.update()
+
+        if self.left_pressed and not self.right_pressed:
+            self.player.move("left")
+        elif self.right_pressed and not self.left_pressed:
+            self.player.move("right")
+        else:
+            self.player.stop()
+
+        self.player.sprint(self.shift_pressed)
+        self.player.update()
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.A:
+            self.left_pressed = True
+        elif key == arcade.key.D:
+            self.right_pressed = True
+        elif key == arcade.key.SPACE:
+            self.player.jump()
+        elif key in (arcade.key.LSHIFT, arcade.key.RSHIFT):
+            self.shift_pressed = True
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.A:
+            self.left_pressed = False
+        elif key == arcade.key.D:
+            self.right_pressed = False
+        elif key in (arcade.key.LSHIFT, arcade.key.RSHIFT):
+            self.shift_pressed = False
+
+
+def main():
+    game = MyGame()
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
